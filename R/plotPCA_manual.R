bbdd7e8d6a137aeb0597197f3e1685976550f858
rogangrant <rogangrant2022@u.northwestern.edu>
2020-06-17 17:55:27 -0500

    added option for extra PCs

--- R/plotPCA_manual.R
+++ R/plotPCA_manual.R
@@ -1,5 +1,5 @@
 #copy of DESeq2 plotPCA, but returns the actual PCA object
-plotPCA_manual = function(object, intgroup="condition", ntop=500)
+plotPCA_manual = function(object, intgroup="condition", ntop=500, pcs = 2)
 {
   # calculate the variance for each gene
   rv <- rowVars(assay(object))
@@ -27,8 +27,11 @@ plotPCA_manual = function(object, intgroup="condition", ntop=500)
   }
   
   # assembly the data for the plot
-  d <- data.frame(PC1=pca$x[,1], PC2=pca$x[,2], group=group, intgroup.df, name=colnames(object))
-  attr(d, "percentVar") <- percentVar[1:2]
+  d <- data.frame(group=group, intgroup.df, name=colnames(object))
+  tmp = pca$x[, 1:pcs]
+  colnames(tmp) = paste0("PC", 1:pcs)
+  d = cbind(tmp, d)
+  attr(d, "percentVar") <- percentVar[1:pcs]
   
   plt = ggplot(data=d, aes_string(x="PC1", y="PC2", color="group")) + geom_point(size=3) + 
     xlab(paste0("PC1: ",round(percentVar[1] * 100),"% variance")) +
