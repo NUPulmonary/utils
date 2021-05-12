a46b9c17c3eff4991650a604e0f44ad1611c3c39
rogangrant <rogangrant2022@u.northwestern.edu>
2021-05-12 12:44:22 -0500

    added output for loadings and percent variance

--- R/plotPCA_manual.R
+++ R/plotPCA_manual.R
@@ -1,10 +1,18 @@
-#copy of DESeq2 plotPCA, but returns the actual PCA object
-#one addition: merge_metadata, which joins output dataframe with colData. Defaults to FALSE for compatibility reasons.
+# copy of DESeq2 plotPCA, but returns the actual PCA object
+# add-ins
+# now returns percent variance for each PC requested
+# merge_metadata: joins output dataframe with colData. Defaults to FALSE for compatibility reasons.
+# return_loadings: return gene loadings data frame. Defaults to FALSE for compatibility reasons.
+# mart_name: prefix for biomart dataset name. Defaults to "mmusculus". Ignored if custom annotation is supplied or return_loadings is FALSE.
+# custom_annotation: a dataframe with "ensembl_gene_id" and "external_gene_name" as columns. Take precedence over mart name if supplied.
 plotPCA_manual = function(object, 
                           intgroup="condition", #essentially ignored if merge_metadata = TRUE
                           ntop=500, 
                           pcs = 2,
-                          merge_metadata = FALSE)
+                          merge_metadata = FALSE,
+                          return_loadings = FALSE,
+                          mart_name = "mmusculus",
+                          custom_annotation = NULL)
 {
   require(tidyverse)
   
@@ -33,7 +41,7 @@ plotPCA_manual = function(object,
     colData(object)[[intgroup]]
   }
   
-  # assembly the data for the plot
+  # assemble the data for the plot
   d <- data.frame(group=group, intgroup.df, name=colnames(object))
   tmp = pca$x[, 1:pcs]
   colnames(tmp) = paste0("PC", 1:pcs)
@@ -58,5 +66,33 @@ plotPCA_manual = function(object,
     ylab(paste0("PC2: ",round(percentVar[2] * 100),"% variance")) +
     coord_fixed()
   
-  return(list(pca = pca, data = d, plot = plt))
+  #if requested, return loadings in human-readable format
+  if(return_loadings == TRUE)
+  {
+    if(is.null(custom_annotation))
+    {
+      require(biomaRt)
+      mart = useMart("ensembl", paste0(mart_name, "_gene_ensembl"))
+      conv = getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
+                   mart = mart)
+    } else
+    {
+      conv = custom_annotation
+    }
+    
+    loadings = pca$rotation %>% 
+      as.data.frame() %>% 
+      rownames_to_column("ensembl_gene_id") %>% 
+      left_join(., conv) %>% 
+      dplyr::relocate(ensembl_gene_id, external_gene_name) %>% 
+      arrange(desc(PC1))
+  } else
+  {
+    loadings = NULL
+  }
+  
+  #output percent variance for each PC as a tidy data frame
+  percent_var = data.frame(component = 1:pcs, percent_var = round(percentVar[1:pcs] * 100))
+  
+  return(list(pca = pca, data = d, plot = plt, loadings = loadings, percent_var = percent_var))
 }
