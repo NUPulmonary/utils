7795bc34413b9a18153a425ded3622cc9b538528
rogangrant <rogangrant2022@u.northwestern.edu>
2021-12-19 22:31:49 -0600

    fixed handling of missing files, added status messages

--- R/merge_10X_BAMs.R
+++ R/merge_10X_BAMs.R
@@ -5,7 +5,7 @@
 #' @param samples_of_interest vector of sample names to combine, e.g. SC001. If NULL, takes all from first directory.
 #' @param output_dir directory to output merged BAM files
 #' @param identity_table a dataframe with ncol = length(input_dirs) and nrow = length(samples of interest). Rownames denote final library name, rows are vectors of equivalent samples. If NULL, done automatically by name
-#' @param overwrite_files whether or not to overwrite destination BAM files. Defaults to TRUE 
+#' @param overwrite_files whether or not to overwrite destination BAM files. Defaults to TRUE
 #' @export
 
 merge_10X_BAMs = function(input_dirs,
@@ -20,6 +20,7 @@ merge_10X_BAMs = function(input_dirs,
   if(!dir.exists(output_dir))
   {
     dir.create(output_dir, recursive = T)
+    message("Output directory created")
   }
   
   #if samples are not specified, take all from first directory
@@ -36,15 +37,16 @@ merge_10X_BAMs = function(input_dirs,
     for(i in 1:length(samples_of_interest))
     {
       sample = samples_of_interest[i]
-      cur = paste(input_dirs, sample, sep = "/")
+      cur = paste(input_dirs, sample, "outs", "possorted_genome_bam.bam", sep = "/")
       identity_table[i, ] = cur
     }
     rownames(identity_table) = samples_of_interest
   }
   
   #ensure all of the paths are valid
-  all_files = as.vector(identity_table)
-  valid_vector = vapply(all_files, dir.exists, FUN.VALUE = TRUE)
+  #ignore NAs because these are by definition files that don't exist
+  all_files = as.character(na.omit(as.vector(identity_table)))
+  valid_vector = vapply(all_files, file.exists, FUN.VALUE = TRUE)
   if(any(valid_vector == FALSE))
   {
     error_message = paste0("following pilepaths are invalid\n", 
@@ -55,9 +57,11 @@ merge_10X_BAMs = function(input_dirs,
   #if all is well, merge the bam files
   for(i in 1:nrow(identity_table))
   {
-    files_vector = identity_table[i, ]
+    #NAs are files that should not be merged/don't exist; remove
+    files_vector = as.character(na.omit(identity_table[i, ]))
     sample = rownames(identity_table)[i]
+    message(paste0("Merging sample ", sample))
     mergeBam(files = files_vector,
-             destination = paste0(output_dir, "/", sample, ".bam"))
+             destination = paste0(output_dir, "/", sample, "_possorted_genome_bam.bam"))
   }
 }
